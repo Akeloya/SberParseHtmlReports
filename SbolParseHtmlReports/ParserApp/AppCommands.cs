a094b755ab608681b3c309c274e82f9559e5c565
Maxim <yugov.max@mail.ru>
2021-03-16 23:16:04 +0700

    Добавление команд

--- SbolParseHtmlReports/ParserApp/AppCommands.cs
+++ SbolParseHtmlReports/ParserApp/AppCommands.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Input;
 
 namespace ParserApp
@@ -22,5 +18,20 @@ namespace ParserApp
             new InputGestureCollection(
                 new List<InputGesture>() { new KeyGesture(Key.F6) })
             );
+
+        public static readonly RoutedCommand OpenHtmlFile = new RoutedCommand(
+            nameof(OpenHtmlFile),
+            typeof(MainWindow),
+            new InputGestureCollection(
+                new List<InputGesture>() { new KeyGesture(Key.O, ModifierKeys.Control) })
+            );
+
+        public static readonly RoutedCommand OpenCsvFile = new RoutedCommand(
+            nameof(OpenCsvFile),
+            typeof(MainWindow),
+            new InputGestureCollection(
+                new List<InputGesture>() { new KeyGesture(Key.O, ModifierKeys.Shift | ModifierKeys.Control) })
+            );
+
     }
 }
