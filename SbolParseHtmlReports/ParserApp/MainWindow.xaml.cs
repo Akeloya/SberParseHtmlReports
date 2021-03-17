b8af248898cd819fb3196b7602c0d521380995d7
Maxim <yugov.max@mail.ru>
2021-03-17 23:52:32 +0700

    Заготовка печати таблицы

--- SbolParseHtmlReports/ParserApp/MainWindow.xaml.cs
+++ SbolParseHtmlReports/ParserApp/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using ParserCore;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Linq;
 using System.IO;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace ParserApp
 {
@@ -70,7 +73,7 @@ namespace ParserApp
 
         private void OpenCsvFile_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            
         }
 
         private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -94,5 +97,58 @@ namespace ParserApp
             e.CanExecute = _parser != null && _parser.Operations.Any();
         }
 
+        private void Print_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                var flowDoc = new FlowDocument();
+                var table = new Table();                
+                int numberOfColumns = 8;
+                for (int x = 0; x < numberOfColumns; x++)
+                {
+                    table.Columns.Add(new TableColumn());
+
+                    // Set alternating background colors for the middle colums.
+                    if (x % 2 == 0)
+                        table.Columns[x].Background = Brushes.Beige;
+                    else
+                        table.Columns[x].Background = Brushes.LightSteelBlue;                    
+                }
+                var titleGroup = new TableRowGroup();
+                var titleRow = new TableRow();
+                titleGroup.Rows.Add(titleRow);
+
+                titleRow.Background = Brushes.Silver;
+                titleRow.FontSize = 40;
+                titleRow.FontWeight = FontWeights.Bold;
+                titleRow.Cells.Add(new TableCell(new Paragraph(new Run("Данные отчета СБОЛ"))));
+                titleRow.Cells[0].ColumnSpan = 6;
+
+                table.RowGroups.Add(titleGroup);
+                var tgroup = new TableRowGroup();
+                foreach(var item in _parser.Operations)
+                {
+                    var tRow = new TableRow
+                    {
+                        FontSize = 12,
+                        FontWeight = FontWeights.Normal
+                    };
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.RowNumber.ToString()))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.Date.ToString()))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.Category))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.Title))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.Summ.ToString()))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.BalanceAfter.ToString()))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.Location))));
+                    tRow.Cells.Add(new TableCell(new Paragraph(new Run(item.ProcessDate.ToString()))));
+                    
+                    tgroup.Rows.Add(tRow);
+                }
+                table.RowGroups.Add(tgroup);
+                flowDoc.Blocks.Add(table);
+                printDialog.PrintDocument(((IDocumentPaginatorSource)flowDoc).DocumentPaginator, "Csv");
+            }
+        }
     }
 }
